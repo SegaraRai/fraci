5f3cec83888bc5039b81cee6b410a93a6cb09358
SegaraRai <SegaraRai@users.noreply.github.com>
2025-03-08 22:01:56 +0900

    fix: disuse unique symbol to brand `FractionalIndex` to avoid TS4058 error

---
+++ .changeset/fair-lamps-type.md
@@ -0,0 +1,5 @@
+---
+"fraci": minor
+---
+
+Do not use unique symbol to brand `FractionalIndex` to avoid TS4058 error.
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,8 @@
-declare const fractionalIndexBrand: unique symbol;
-
 /**
  * A branded string type representing a fractional index.
  *
  * **WARNING: DO NOT cast untrusted values to `FractionalIndex`, or your service may encounter runtime exceptions.**
  */
 export type FractionalIndex<D extends string, L extends string, X> = string & {
-  readonly [fractionalIndexBrand]: [D, L, X];
+  readonly __fraci__: [D, L, X];
 };
